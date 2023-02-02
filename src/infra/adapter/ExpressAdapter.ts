3f89960228cdf6c445f660edf9a14bf369c01ba2
Wan Lucas <wan.lucas.90@gmail.com>
2023-02-02 19:12:37 -0300

    feat: express adapter

---
+++ src/infra/adapter/ExpressAdapter.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { IRequest, IResponse } from '../../application/interface/http';
+
+export default class ExpressAdapter {
+  static create(controller: Function) {
+    return async (req: Request, res: Response) => {
+      const { body, params, query } = req;
+      const request: IRequest = { body, params, query };  
+      const { status, payload }: IResponse = await controller(request);
+
+      return res.status(status).json(payload);
+    };
+  }
+}
