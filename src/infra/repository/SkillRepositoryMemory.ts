b5ca99cf4d8d54da55462171d90f522bb6e9d978
Wan Lucas <wan.lucas.90@gmail.com>
2023-02-02 19:13:33 -0300

    feat(new repo): Skill repo in memory

---
+++ src/infra/repository/SkillRepositoryMemory.ts
@@ -0,0 +1,23 @@
+import Skill from "../../domain/entity/Skill";
+import SkillRepository from "../../domain/repository/SkillRepository";
+
+export default class SkillRepositoryMemory implements SkillRepository {
+  public readonly skills = [
+    { id: 1, entirety: 1,  name: 'Typescript', xp: 100 },
+    { id: 2, entirety: 1,  name: 'Node.JS', xp: 200 },
+    { id: 3, entirety: 1,  name: 'MySQL', xp: 120 },
+  ];
+  
+  constructor() { }
+
+  async getSkills(): Promise<Skill[]> {
+    return this.skills;
+  }
+
+  async saveSkill(skill: Skill): Promise<number> {
+    const id = this.skills.length + 1;
+    this.skills.push(({ ...skill, id }));
+
+    return id;
+  }
+}
